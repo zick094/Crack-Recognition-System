5a35dd433146f4f034148c84222771783dfe9dfb
zick094 <luca.riccardi@live.it>
2020-05-29 14:41:31 +0200

    App script

--- app.py
+++ app.py
@@ -61,6 +61,9 @@ def analyze():
             <a href="https://www.google.com/search?q=concrete+crack+images&sxsrf=ALeKk01yyQFqps4vcbwaI8I3z6tIubiaNw:1590330445607&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjUq6TK2szpAhVCzqYKHTMQAEkQ_AUoAXoECAsQAw&biw=2064&bih=1054", target = "_blank">search for cracked concrete images</a>
             <br>
             <a href="https://www.google.com/search?q=plain+concrete&tbm=isch&ved=2ahUKEwiYq4Tf2szpAhVYt6QKHXWmAScQ2-cCegQIABAA&oq=plain+concrete&gs_lcp=CgNpbWcQAzIECCMQJzIECAAQEzIECAAQEzIECAAQEzIECAAQEzIECAAQEzIECAAQEzIECAAQEzIECAAQEzIECAAQE1CnOVipOmDMO2gAcAB4AIABXIgBtgGSAQEymAEAoAEBqgELZ3dzLXdpei1pbWc&sclient=img&ei=eYTKXtiKB9jukgX1zIa4Ag&bih=1054&biw=2064", target = "_blank">search for plain concrete images</a>
+            <br>
+            <br>            
+            <iframe src="https://youtube.com/embed/NafEYsmeBGU" width="853" height="480" frameborder="0" allowfullscreen></iframe>
             ''')
             
             
