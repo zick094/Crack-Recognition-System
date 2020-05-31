ebc358978252f6ed5e947dab8bced90a5937fd83
zick094 <luca.riccardi@live.it>
2020-05-31 19:50:02 +0200

    added files

--- app.py
+++ app.py
@@ -101,11 +101,11 @@ def uploaded_file(filename):
 def main():
     load()
     #this for local debugging
-    app.run(debug=True)
+    #app.run(debug=True)
     
     #this for remote working
-    #port = int(os.environ.get('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
     main()
