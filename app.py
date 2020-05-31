2331d62ae1beda01eded4cd163fc3879dc5f46db
zick094 <luca.riccardi@live.it>
2020-05-31 14:59:20 +0200

    added files

--- app.py
+++ app.py
@@ -1,78 +1,105 @@
-
+from flask import Flask, render_template, request, redirect, session, send_from_directory, url_for
+import requests
 
 import tensorflow as tf
-
-
 import numpy as np
-
 
 from io import BytesIO
 import os
+from PIL import Image
 
-from flask import Flask, request
+from werkzeug.utils import secure_filename
+
 
 app = Flask(__name__)
 
-dict_values = {0:'no cracks',1:'yes cracks'}
+
+# setting up for uploading pics
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+#define function for check file extension
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+#lets add folder to app.config
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+#dictionary for translating prediction results
+dict_values = {0:'There are no cracks in the picture.',1:'There are cracks in the picture.'}
 
 
 def load():
     global services 
     services = dict()
     
-    services['model'] = tf.keras.models.load_model('./resnet_trained.h5')
+    services['model'] = tf.keras.models.load_model('resnet_trained.h5')
     
-    
-    #services['graph'] = tf.compat.v1.get_default_graph()
-    
+
     
 @app.route('/', methods =['GET','POST'])
+@app.route('/home', methods =['GET','POST'])
+def home_page():
+    return render_template('index.html')
+
+
+
+@app.route('/tool', methods =['GET','POST'])
 def analyze():
     if request.method == 'POST':
         
+
         URL = request.form.get('url')
-        
+        file = request.files['file']
+
+        if not URL:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                img = Image.open(file)
+                services['to_print'] = url_for('uploaded_file',filename=filename)
+            else:
+                print('Wrong format or not uploaded picture')
+                return redirect('\home')
+        else:
+            response = requests.get(URL)
+            img = Image.open(BytesIO(response.content))
+            services['to_print'] = URL
+
+         
         
         # preprocess it to the model format
-        response = requests.get(URL)
-        img = Image.open(BytesIO(response.content))
         image_resized = img.resize((224,224))
         
         
         # predict image
-        #with services['graph'].as_default():
-        prediction = services['model'].predict(np.expand_dims(np.array(image_resized, dtype='float32'), axis=0))
+        global prediction_
+        prediction_ = services['model'].predict(np.expand_dims(np.array(image_resized, dtype='float32'), axis=0))
             
         
-        return ('''
-                <h1> Crack: '''+dict_values[np.argmax(prediction[0])]+''' </h1>
-                <button> <a href='#' onclick='history.go(-1)'>Back</a> </button> <br><br>
-                <img src='''+URL+'''> <br>
-                ''')
-    
-    return ('''
-            <form method="post">
-            Paste url concrete cracks: <br>
-            URL: <input type="text" name="url"> <br> <br>
-            <input type="submit" value="Submit"> <br>
-            </form>
-            <br><br>
-            <a href="https://www.google.com/search?q=concrete+crack+images&sxsrf=ALeKk01yyQFqps4vcbwaI8I3z6tIubiaNw:1590330445607&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjUq6TK2szpAhVCzqYKHTMQAEkQ_AUoAXoECAsQAw&biw=2064&bih=1054", target = "_blank">search for cracked concrete images</a>
-            <br>
-            <a href="https://www.google.com/search?q=plain+concrete&tbm=isch&ved=2ahUKEwiYq4Tf2szpAhVYt6QKHXWmAScQ2-cCegQIABAA&oq=plain+concrete&gs_lcp=CgNpbWcQAzIECCMQJzIECAAQEzIECAAQEzIECAAQEzIECAAQEzIECAAQEzIECAAQEzIECAAQEzIECAAQEzIECAAQE1CnOVipOmDMO2gAcAB4AIABXIgBtgGSAQEymAEAoAEBqgELZ3dzLXdpei1pbWc&sclient=img&ei=eYTKXtiKB9jukgX1zIa4Ag&bih=1054&biw=2064", target = "_blank">search for plain concrete images</a>
-            <br>
-            <a href="https://github.com/zick094/crack_recognition_api" target = "_blank">GitHub Repository</a>
-            <br> 
-            <br>           
-            <iframe src="https://youtube.com/embed/NafEYsmeBGU" width="853" height="480" frameborder="0" allowfullscreen></iframe>
-            ''')
+        return redirect('/prediction')
+
+    else:
+        return render_template('request.html')
             
-            
-            
+@app.route('/prediction', methods = ['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        return redirect('/home')
+    else:
+        result = dict_values[np.argmax(prediction_[0])]
+        return render_template('prediction.html', prediction=result, img=services['to_print'])
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
 def main():
     load()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
+    #port = int(os.environ.get('PORT', 5000))
+    #app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
     main()
